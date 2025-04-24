abeaafe7df33a75e579caa90ea13b03c1e9eff84
Janiele-Machado <janiele.farias@estudante.ifgoiano.edu.br>
2025-04-24 12:48:09 -0300

    Criação da classe conexão
    Essa classe será responsável por estabelecer a conexão com o banco de dados, ela possui atributos estáticos de servidor, senha e usuário, com a senha ocultada para ser adaptada pela senha real de cada banco posteriormente. Há tratamento de exceção para o caso de erro ao conectar, facilitando o encontro do erro e a correção do código.

---
+++ ProjetoAcademia/src/main/java/Academia/Conexao.java
@@ -0,0 +1,33 @@
+
+package Academia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Janiele
+ */
+public class Conexao {
+    
+    public static final String SERVIDOR = "jdbc:mysql://localhost:3306/academia_db";
+    public static final String USUARIO = "root";
+    public static final String SENHA = "******";
+    
+    
+    public Connection getConexao()throws SQLException {
+        try {
+            Connection conexao = DriverManager.getConnection(SERVIDOR, USUARIO, SENHA);
+            if (conexao != null) {
+                System.out.println("Conexão bem-sucedida com o banco de dados!");
+            }
+            return conexao;
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar: " + e.getMessage());
+            throw e;  // Relança a exceção para que ela possa ser tratada externamente
+        }
+        
+    }
+    
+}
