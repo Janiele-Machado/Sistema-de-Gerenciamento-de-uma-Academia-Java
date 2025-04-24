43ce9c5c8ee1cebb518174386581a29c63982af4
Matheus Felipe <matheus.bastos@estudante.ifgoiano.edu.br>
2025-04-24 15:47:32 -0300

    Criando Atributo e Get/Set de Personal
    Criei o atributo único de Personal e fiz os respectivos get e set do atributo privado.

--- ProjetoAcademia/src/main/java/Academia/Personal.java
+++ ProjetoAcademia/src/main/java/Academia/Personal.java
@@ -1,10 +1,15 @@
-
 package Academia;
 
-/**
- *
- * @author Janiele
- */
 public class Personal extends Usuario {
+    
+        private String especialidade;
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
     
 }
