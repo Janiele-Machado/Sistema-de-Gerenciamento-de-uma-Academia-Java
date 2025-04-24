c4dabf828387016ca9003992670ab0bb2f998b39
Heitor Messias Gomes <heitormessias13@gmail.com>
2025-04-24 19:56:09 -0300

    Retirando os Set da Classe

--- ProjetoAcademia/src/main/java/Academia/Usuario.java
+++ ProjetoAcademia/src/main/java/Academia/Usuario.java
@@ -31,56 +31,29 @@ public abstract class Usuario {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getNome() {
         return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 
     public String getCpf() {
         return cpf;
     }
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getDataNasc() {
         return dataNasc;
     }
 
-    public void setDataNasc(String dataNasc) {
-        this.dataNasc = dataNasc;
-    }
-
     public String getSenha() {
         return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
     }
 
     public String getTipo() {
         return tipo;
     }
 
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
 
 }
