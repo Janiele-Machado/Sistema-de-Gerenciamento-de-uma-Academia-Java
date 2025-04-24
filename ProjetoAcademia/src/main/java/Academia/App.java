85eaaba21359e0d3cb7ad80b508903d87d0fee23
Janiele-Machado <janiele.farias@estudante.ifgoiano.edu.br>
2025-04-24 12:25:05 -0300

    Criação do Menu de cadastro
    Criação do método do menu de cadastro  e da estrutura de repetição do segmento dele

--- ProjetoAcademia/src/main/java/Academia/App.java
+++ ProjetoAcademia/src/main/java/Academia/App.java
@@ -3,22 +3,29 @@ package Academia;
 import java.util.Scanner;
 
 public class App {
+
     public static void main(String[] args) {
         Metodos metodos = new Metodos();
         Scanner scan = new Scanner(System.in);
         int opc_principal = 0;
-        
-        while(opc_principal != 3){
-            
+
+        while (opc_principal != 3) {
+
             metodos.menuPrincipal();
             opc_principal = scan.nextInt();
-            
+
             if (opc_principal == 1) {
-                
-            }else if (opc_principal == 2) {
-                
+                int opc_cadastro = 0;
+                while (opc_cadastro != 4) {
+
+                    metodos.menuCadastro();
+                    opc_cadastro = scan.nextInt();
+                }
+
+            } else if (opc_principal == 2) {
+
             } else if (opc_principal == 3) {
-                
+
                 System.out.println("Obrigado por utilizar nosso sistema <3 ");
             }
         }
--- ProjetoAcademia/src/main/java/Academia/Metodos.java
+++ ProjetoAcademia/src/main/java/Academia/Metodos.java
@@ -1,4 +1,3 @@
-
 package Academia;
 
 /**
@@ -6,15 +5,27 @@ package Academia;
  * @author Janiele
  */
 public class Metodos {
-    
-    public void menuPrincipal(){
+
+    public void menuPrincipal() {
         //Função repeat utilizada para otimizar a decoração 
-        System.out.println("-".repeat(30));
-        System.out.println("Seja Bem Vindo ao Nosso Sistema de Academia");
-        System.out.println("Escolha uma Opcao:");
-        System.out.println("1- Cadastrar");
-        System.out.println("2- Logar");
-        System.out.println("3- Sair");
-        System.out.println("-".repeat(30));
+        System.out.println("-".repeat(44)); //A funçao repete o que está dentro das aspas
+        System.out.println("|Seja Bem Vindo ao Nosso Sistema de Academia|");
+        System.out.println("|Escolha uma Opcao:                         |");
+        System.out.println("|1- Cadastrar                               |");
+        System.out.println("|2- Logar                                   |");
+        System.out.println("|3- Sair                                    |");
+        System.out.println("-".repeat(44));
     }
+
+    public void menuCadastro() {
+        //Função repeat utilizada para otimizar a decoração 
+        System.out.println("-".repeat(33));
+        System.out.println("|Escolha o seu tipo de Cadastro:|");
+        System.out.println("|1- Sou Aluno                   |");
+        System.out.println("|2- Sou Admistrador             |");
+        System.out.println("|3- Sou Personal                |");
+        System.out.println("|4- Quero Sair                  |");
+        System.out.println("-".repeat(33));
+    }
+
 }
