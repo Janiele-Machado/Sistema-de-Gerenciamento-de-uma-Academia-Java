326f7c4370b420bb752b98518c84741245e51a98
Janiele-Machado <janiele.farias@estudante.ifgoiano.edu.br>
2025-04-24 14:21:10 -0300

    Update Banco
    Banco atualizado com algumas modificações

---
+++ academiaAtualizado1.sql
@@ -0,0 +1,269 @@
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: localhost    Database: academia_db
+-- ------------------------------------------------------
+-- Server version	8.0.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `adm`
+--
+
+DROP TABLE IF EXISTS `adm`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `adm` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `salario` decimal(10,2) DEFAULT NULL,
+  `telefone_comercial` varchar(20) DEFAULT NULL,
+  `descricao` text,
+  `setor` varchar(20) DEFAULT 'geral',
+  `fk_usu_adm` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_usu_adm` (`fk_usu_adm`),
+  CONSTRAINT `fk_usu_adm` FOREIGN KEY (`fk_usu_adm`) REFERENCES `usuario` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `adm`
+--
+
+LOCK TABLES `adm` WRITE;
+/*!40000 ALTER TABLE `adm` DISABLE KEYS */;
+INSERT INTO `adm` VALUES (1,4500.00,'(48) 99999-8888','Administrador geral da unidade','geral',3);
+/*!40000 ALTER TABLE `adm` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `agendamento_aula`
+--
+
+DROP TABLE IF EXISTS `agendamento_aula`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `agendamento_aula` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `assinatura_id` int DEFAULT NULL,
+  `data_hora` datetime DEFAULT NULL,
+  `duracao_min` int DEFAULT '60',
+  `status` enum('agendado','cancelado','realizado') DEFAULT 'agendado',
+  `observacoes` text,
+  PRIMARY KEY (`id`),
+  KEY `assinatura_id` (`assinatura_id`),
+  CONSTRAINT `agendamento_aula_ibfk_1` FOREIGN KEY (`assinatura_id`) REFERENCES `assinatura` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `agendamento_aula`
+--
+
+LOCK TABLES `agendamento_aula` WRITE;
+/*!40000 ALTER TABLE `agendamento_aula` DISABLE KEYS */;
+INSERT INTO `agendamento_aula` VALUES (1,1,'2025-04-20 16:47:27',60,'agendado','Primeira avaliação física'),(2,1,'2025-04-22 16:47:27',60,'agendado','Treino de força inicial');
+/*!40000 ALTER TABLE `agendamento_aula` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `aluno`
+--
+
+DROP TABLE IF EXISTS `aluno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `aluno` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `status` varchar(20) DEFAULT 'ativo',
+  `matricula` varchar(20) DEFAULT NULL,
+  `objetivo` text,
+  `fk_usu_aluno` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `matricula` (`matricula`),
+  KEY `fk_usu_aluno` (`fk_usu_aluno`),
+  CONSTRAINT `fk_usu_aluno` FOREIGN KEY (`fk_usu_aluno`) REFERENCES `usuario` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aluno`
+--
+
+LOCK TABLES `aluno` WRITE;
+/*!40000 ALTER TABLE `aluno` DISABLE KEYS */;
+INSERT INTO `aluno` VALUES (1,'ativo','MAT2023001','Perder peso e ganhar resistência',1);
+/*!40000 ALTER TABLE `aluno` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `assinatura`
+--
+
+DROP TABLE IF EXISTS `assinatura`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `assinatura` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `aluno_id` int DEFAULT NULL,
+  `personal_id` int DEFAULT NULL,
+  `nome_plano` varchar(100) DEFAULT NULL,
+  `duracao_meses` int DEFAULT NULL,
+  `valor` decimal(10,2) DEFAULT NULL,
+  `data_inicio` date DEFAULT NULL,
+  `data_fim` date DEFAULT NULL,
+  `ativa` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `aluno_id` (`aluno_id`),
+  KEY `personal_id` (`personal_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `assinatura`
+--
+
+LOCK TABLES `assinatura` WRITE;
+/*!40000 ALTER TABLE `assinatura` DISABLE KEYS */;
+INSERT INTO `assinatura` VALUES (1,1,2,'Plano Trimestral',3,450.00,'2025-04-19','2025-07-18',1);
+/*!40000 ALTER TABLE `assinatura` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `avaliacao_personal`
+--
+
+DROP TABLE IF EXISTS `avaliacao_personal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `avaliacao_personal` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `aluno_id` int DEFAULT NULL,
+  `personal_id` int DEFAULT NULL,
+  `nota` int DEFAULT NULL,
+  `comentario` text,
+  `data_avaliacao` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `aluno_id` (`aluno_id`),
+  KEY `personal_id` (`personal_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `avaliacao_personal`
+--
+
+LOCK TABLES `avaliacao_personal` WRITE;
+/*!40000 ALTER TABLE `avaliacao_personal` DISABLE KEYS */;
+INSERT INTO `avaliacao_personal` VALUES (1,1,2,5,'Muito atencioso e profissional!','2025-04-19 13:48:38');
+/*!40000 ALTER TABLE `avaliacao_personal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `financa`
+--
+
+DROP TABLE IF EXISTS `financa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `financa` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `usuario_id` int DEFAULT NULL,
+  `valor` decimal(10,2) DEFAULT NULL,
+  `data_pagamento` date DEFAULT NULL,
+  `categoria` enum('mensalidade','bônus','salario','outros') DEFAULT NULL,
+  `saldo` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuario_id` (`usuario_id`),
+  CONSTRAINT `financa_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `financa`
+--
+
+LOCK TABLES `financa` WRITE;
+/*!40000 ALTER TABLE `financa` DISABLE KEYS */;
+INSERT INTO `financa` VALUES (1,1,450.00,'2025-04-19','mensalidade',100.00),(2,2,200.00,'2025-04-19','bônus',3500.00),(3,3,4500.00,'2025-04-19','salario',5000.00);
+/*!40000 ALTER TABLE `financa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `personal`
+--
+
+DROP TABLE IF EXISTS `personal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `personal` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `salario` decimal(10,2) DEFAULT NULL,
+  `bonus_por_aluno` decimal(10,2) DEFAULT NULL,
+  `especialidade` varchar(100) DEFAULT NULL,
+  `fk_usu_personal` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_usu_personal` (`fk_usu_personal`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personal`
+--
+
+LOCK TABLES `personal` WRITE;
+/*!40000 ALTER TABLE `personal` DISABLE KEYS */;
+INSERT INTO `personal` VALUES (2,3500.00,200.00,'Musculação',2);
+/*!40000 ALTER TABLE `personal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) DEFAULT NULL,
+  `cpf` varchar(14) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `data_nascimento` date DEFAULT NULL,
+  `senha` varchar(255) DEFAULT NULL,
+  `tipo` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `cpf` (`cpf`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'Ana Silva','123.456.789-00','ana@email.com','1990-05-15','senha123','aluno'),(2,'Bruno Costa','321.654.987-00','bruno@email.com','1985-08-10','seguro456','personal'),(3,'Carlos Dias','111.222.333-44','carlos@email.com','1975-01-20','adm123','adm'),(4,'Manuela Hija','299.000.900-88','manu@gmail.com','2000-09-07','mani111','aluno');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-04-24 14:19:41
