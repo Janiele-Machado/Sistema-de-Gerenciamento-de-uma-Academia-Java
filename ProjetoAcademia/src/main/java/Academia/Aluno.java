09d07125291e891b03d6e0b6487ec6ea7d88203e
Janiele-Machado <janiele.farias@estudante.ifgoiano.edu.br>
2025-04-24 14:44:07 -0300

    Criando atributos da classe aluno
    criei os atributos da classe aluno com o limitador de acesso private e cirei gets e sets para conseguir acessar posteriormente com mais segurança.

--- ProjetoAcademia/src/main/java/Academia/Aluno.java
+++ ProjetoAcademia/src/main/java/Academia/Aluno.java
@@ -1,4 +1,3 @@
-
 package Academia;
 
 /**
@@ -6,5 +5,33 @@ package Academia;
  * @author Janiele
  */
 public class Aluno extends Usuario {
-    
+
+    private String status;
+    private String matricula;
+    private String objetivo;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getObjetivo() {
+        return objetivo;
+    }
+
+    public void setObjetivo(String objetivo) {
+        this.objetivo = objetivo;
+    }
+
 }
