837cde9aa9c50d7dda7a0598c678a1ccbb1e5f14
Matheus-fel <samuelantuneso@icloud.com>
2025-04-26 13:50:41 -0300

    Criaçao metodo de logar
    Método logar() solicita email e senha, valida as credenciais no banco usando PreparedStatement e retorna o tipo de usuário se autenticado, retorna null afim de ser tratado em outro metodo

--- ProjetoAcademia/src/main/java/Academia/Metodos.java
+++ ProjetoAcademia/src/main/java/Academia/Metodos.java
@@ -1,10 +1,19 @@
 package Academia;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Scanner;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+
 /**
  *
  * @author Janiele
  */
 public class Metodos {
+    Scanner  scan = new Scanner(System.in);
 
     public void menuPrincipal() {
         //Função repeat utilizada para otimizar a decoração 
@@ -26,6 +35,39 @@ public class Metodos {
         System.out.println("|3- Sou Personal                |");
         System.out.println("|4- Quero Sair                  |");
         System.out.println("-".repeat(33));
+    }
+    
+    public String logar() throws SQLException{
+        System.out.println("Email:");
+        String email_ver = scan.nextLine();
+        System.out.println("Senha:");
+        String senha_vre = scan.nextLine();
+        
+        Connection conexao = new Conexao().getConexao();
+        
+        String sql_logar = "SELECT tipo from `academiaatualizado1.sql`.usuario\n where email = ? AND senha = ?;";
+        PreparedStatement comandoLogar = conexao.prepareStatement(sql_logar);
+        comandoLogar.setString(1, email_ver);
+        comandoLogar.setString(2, senha_vre);
+        ResultSet rs = comandoLogar.executeQuery();
+        
+        if(rs.next()){
+            String tipo_uso = rs.getString("tipo");
+            return tipo_uso;
+        }else{
+            
+            return null;
+                    
+        }
+        
+        
+        
+        
+        
+        
+        
+       
+        
     }
 
 }
