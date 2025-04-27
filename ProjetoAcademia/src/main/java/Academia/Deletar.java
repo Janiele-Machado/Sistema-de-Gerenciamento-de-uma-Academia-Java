81d455c09b1f4b6d8a19f3f916e3c278fd0ded80
Matheus-fel <samuelantuneso@icloud.com>
2025-04-27 09:08:22 -0300

    Creiação classe deletar
    Classe para fazer delete  no banco

---
+++ ProjetoAcademia/src/main/java/Academia/Deletar.java
@@ -0,0 +1,16 @@
+
+package Academia;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author samuel
+ */
+public class Deletar {
+    
+    
+    
+}
