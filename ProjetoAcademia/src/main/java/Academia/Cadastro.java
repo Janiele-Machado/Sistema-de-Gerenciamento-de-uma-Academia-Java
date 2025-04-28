3bfd0ad31f71dac484a8922155c68d1277c017fc
Janiele-Machado <janiele.farias@estudante.ifgoiano.edu.br>
2025-04-28 15:15:07 -0300

    Criação do método inserir um personal
    foi criado um método que cadastra um novo personal no banco de dados

--- ProjetoAcademia/src/main/java/Academia/Cadastro.java
+++ ProjetoAcademia/src/main/java/Academia/Cadastro.java
@@ -101,14 +101,14 @@ public class Cadastro {
             rs.next();
             int uso_adm = rs.getInt(1);
 
-            // Inserindo dados na tabela aluno
-            PreparedStatement comandoAluno = conexao.prepareStatement(sqlAdm);
-            comandoAluno.setDouble(1, adm.getSalario());
-            comandoAluno.setString(2, adm.getNumero_comercial());
-            comandoAluno.setString(3, adm.getDesc());
-            comandoAluno.setString(4, adm.getSetor());
-            comandoAluno.setInt(5, uso_adm);
-            comandoAluno.executeUpdate();
+            // Inserindo dados na tabela adm
+            PreparedStatement comandoAdm = conexao.prepareStatement(sqlAdm);
+            comandoAdm.setDouble(1, adm.getSalario());
+            comandoAdm.setString(2, adm.getNumero_comercial());
+            comandoAdm.setString(3, adm.getDesc());
+            comandoAdm.setString(4, adm.getSetor());
+            comandoAdm.setInt(5, uso_adm);
+            comandoAdm.executeUpdate();
 
             // Confirmar a transação
             conexao.commit();
@@ -116,7 +116,7 @@ public class Cadastro {
 
             // Fechar as conexões
             comandoUsuario.close();
-            comandoAluno.close();
+            comandoAdm.close();
             conexao.setAutoCommit(true);
 
         } catch (Exception e) {
@@ -127,6 +127,66 @@ public class Cadastro {
                 System.out.println("Erro ao reverter a transação: " + rollbackEx.getMessage());
             }
             System.out.println("Erro ao cadastrar adm: " + e.getMessage()); //retorno para o caso de dar erro no processo;
+        }
+    }
+     
+       public void inserir(Personal personal) throws Exception {
+        // conecta com o banco  
+        Connection conexao = new Conexao().getConexao();
+        // Inserir usuário na tabela 'usuarios' (tabela genérica para usuários)
+        //INSERT INTO `academia_db`.`usuario` (`nome`, `cpf`, `email`, `data_nascimento`, `senha`, `tipo`) 
+        //VALUES ('Maria Gomes', '120.890.890-89', 'mary@gmail.com', 
+        //'2005-09-23', 'kika233', 'aluno');
+
+        String sqlUsuario = "INSERT INTO usuario(nome, cpf, email, data_nascimento, senha, tipo) VALUES(?, ?, ?, ?, ?, ?)";
+
+        //INSERT INTO `academia_db`.`personal` (`salario`, `bonus_por_aluno`, `especialidade`, `fk_usu_personal`, `quant_alunos`) VALUES (?,?,?,?,?);
+        String sqlPersonal = "INSERT INTO personal (`salario`, `bonus_por_aluno`, `especialidade`, `fk_usu_personal`, `quant_alunos`) VALUES ( ?, ?, ?, ?, ?)";
+        try {
+            // Começar uma transação para garantir consistência
+            conexao.setAutoCommit(false);
+
+            //inserindo dados na tabela usuários
+            PreparedStatement comandoUsuario = conexao.prepareStatement(sqlUsuario, PreparedStatement.RETURN_GENERATED_KEYS);
+            comandoUsuario.setString(1, personal.getNome());
+            comandoUsuario.setString(2, personal.getCpf());
+            comandoUsuario.setString(3, personal.getEmail());
+            comandoUsuario.setDate(4, personal.getDataNasc());
+            comandoUsuario.setString(5, personal.getSenha());
+            comandoUsuario.setString(6, personal.getTipo());
+            comandoUsuario.executeUpdate();
+
+            // Obter o id gerado automaticamente (pois o banco utiliza auto incremento)
+            ResultSet rs = comandoUsuario.getGeneratedKeys();
+            rs.next();
+            int uso_per = rs.getInt(1);
+
+            // Inserindo dados na tabela personal
+            PreparedStatement comandoPer = conexao.prepareStatement(sqlPersonal);
+            comandoPer.setDouble(1, personal.getSalario());
+            comandoPer.setDouble(2, personal.getBonus_por_aluno());
+            comandoPer.setString(3, personal.getEspecialidade());
+            comandoPer.setInt(4, uso_per);
+            comandoPer.setInt(5, personal.getQtd_aluno());
+            comandoPer.executeUpdate();
+
+            // Confirmar a transação
+            conexao.commit();
+            System.out.println("Administrador cadastrado com sucesso!");
+
+            // Fechar as conexões
+            comandoUsuario.close();
+            comandoPer.close();
+            conexao.setAutoCommit(true);
+
+        } catch (Exception e) {
+            try {
+                // Em caso de erro, desfaz a transação
+                conexao.rollback();
+            } catch (SQLException rollbackEx) {
+                System.out.println("Erro ao reverter a transação: " + rollbackEx.getMessage());
+            }
+            System.out.println("Erro ao cadastrar personal: " + e.getMessage()); //retorno para o caso de dar erro no processo;
         }
     }
 }
