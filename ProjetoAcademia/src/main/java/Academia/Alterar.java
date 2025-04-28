0f2f72e7f9ce7866b26796e20f1f32f8b8e908a2
Heitor Messias Gomes <heitormessias13@gmail.com>
2025-04-28 19:41:09 -0300

    Criando metódo alterarAluno

--- ProjetoAcademia/src/main/java/Academia/Alterar.java
+++ ProjetoAcademia/src/main/java/Academia/Alterar.java
@@ -1,4 +1,10 @@
 package Academia;
 
 public class Alterar {
+    public void alterarAluno(Aluno aluno,int id, String nome, String cpf, String email, String dataNasc,
+        String senha, String tipo, String status, String matricula, String Objetivo) {
+
+        aluno.setMatricula(matricula);
+        aluno.setObjetivo(Objetivo);
+        aluno.setStatus(status);
 }
