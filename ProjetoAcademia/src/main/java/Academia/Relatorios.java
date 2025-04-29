8f6e26e154d2fda4329cba10f1f3711c66ff9d11
Matheus-fel <samuelantuneso@icloud.com>
2025-04-28 21:40:43 -0300

    Metodo Relatorio_personal
    Implementado método que gera relatório dos personal trainers com dados do usuário, como nome, email, CPF, nascimento, salário e especialidade.

--- ProjetoAcademia/src/main/java/Academia/Relatorios.java
+++ ProjetoAcademia/src/main/java/Academia/Relatorios.java
@@ -1,10 +1,44 @@
-
 package Academia;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  *
  * @author samue
  */
 public class Relatorios {
-    
+
+    public void Relatorio_personal() throws SQLException {
+        Connection conexao = new Conexao().getConexao();
+        String sql_relP = " Select * from usuario inner join personal on  usuario.id = personal.fk_usu_personal;";
+        PreparedStatement comando_rel = conexao.prepareStatement(sql_relP);
+        ResultSet rs = comando_rel.executeQuery();
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            String nome = rs.getString("nome");
+            String email = rs.getString("email");
+            String cpf = rs.getString("cpf");
+            Date dataNascimento = rs.getDate("data_nascimento");
+            double salario = rs.getDouble("salario");
+            String especialidade = rs.getString("especialidade");
+
+            System.out.println("ID: " + id
+                    + ", Nome: " + nome
+                    + ", Email: " + email
+                    + ", CPF: " + cpf
+                    + ", Nascimento: " + dataNascimento
+                    + ", Salário: " + salario
+                    + ", Especialidade: " + especialidade);
+
+        }
+        rs.close();
+
+        conexao.close();
+
+    }
+
 }
