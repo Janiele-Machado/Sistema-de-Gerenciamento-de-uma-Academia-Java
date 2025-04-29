954518a2f9fdbac089a03259055522fcc049732f
Matheus Felipe <matheus.bastos@estudante.ifgoiano.edu.br>
2025-04-28 22:57:03 -0300

    Criação do Método alterarPersonal
    Criei na classe Alterar o método alterarPersonal, que fará a inserção dos novos dados do instrutor.

--- ProjetoAcademia/src/main/java/Academia/Alterar.java
+++ ProjetoAcademia/src/main/java/Academia/Alterar.java
@@ -1,8 +1,11 @@
 package Academia;
 
+import java.sql.Date;
+
 public class Alterar {
-    public void alterarAluno(Aluno aluno,int id, String nome, String cpf, String email, String dataNasc,
-        String senha, String tipo, String status, String matricula, String Objetivo) {
+
+    public void alterarAluno(Aluno aluno, int id, String nome, String cpf, String email, String dataNasc,
+            String senha, String tipo, String status, String matricula, String Objetivo) {
 
         aluno.setMatricula(matricula);
         aluno.setObjetivo(Objetivo);
@@ -14,5 +17,20 @@ public class Alterar {
         aluno.setDataNasc(dataNasc);
         aluno.setSenha(senha);
         aluno.setTipo(tipo);
+    }
+
+    public void alterarPersonal(Personal personal, int id, String nome, String cpf, String email, Date dataNasc, 
+            String senha, String tipo, String especialidade, double salario, double bonus_por_aluno) {
+        
+        personal.setNome(nome);
+        personal.setCpf(cpf);
+        personal.setEmail(email);
+        personal.setDataNasc(tipo);
+        personal.setSenha(senha);
+        personal.setTipo(tipo);
+        personal.setBonus_por_aluno(bonus_por_aluno);
+        personal.setEspecialidade(especialidade);
+        personal.setSalario(salario);
+        
     }
 }
