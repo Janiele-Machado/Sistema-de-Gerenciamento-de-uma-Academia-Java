6d6a736ac8a62bd1ee4387c7dc040131567fe247
Matheus Felipe <matheus.bastos@estudante.ifgoiano.edu.br>
2025-04-30 12:58:23 -0300

    Criação do Método de Atualização de Personal
    Criei um método usando sobrecarga que atualiza os dados do personal nas tabelas de personal e usuario no banco de dados.

--- ProjetoAcademia/src/main/java/Academia/Alterar.java
+++ ProjetoAcademia/src/main/java/Academia/Alterar.java
@@ -1,6 +1,8 @@
 package Academia;
 
-import java.sql.Date;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Alterar {
 
@@ -19,16 +21,58 @@ public class Alterar {
         aluno.setTipo(tipo);
     }
 
-    public void alterarPersonal(Personal personal, int id, String nome, String cpf, String email, Date dataNasc, 
+    public void alterarPersonal(Personal personal, int id, String nome, String cpf, String email, String dataNasc, 
             String senha, String tipo, String especialidade) {
         
         personal.setNome(nome);
         personal.setCpf(cpf);
         personal.setEmail(email);
-        personal.setDataNasc(tipo);
+        personal.setDataNasc(dataNasc);
         personal.setSenha(senha);
         personal.setTipo(tipo);
         personal.setEspecialidade(especialidade);
+        
+    }
+    
+    public void alterarPersonal(Personal personal) throws SQLException {
+        
+        Connection conexao = new Conexao().getConexao();
+        
+        String sqlAlterar = "UPDATE  usuarios  SET nome=?, cpf=?, email=?, data_nascimento=?, senha=?, tipo=? WHERE  usuario.id=?";
+        String sqlAlterarPersonal = "UPDATE personal SET especialidade=? WHERE fk_usu_personal=?";
+        
+        try {
+            
+            //Parte de atualizar a tabela usuários
+            PreparedStatement comandoUsuario = conexao.prepareStatement(sqlAlterar);
+
+            comandoUsuario.setString(1, personal.getNome());
+            comandoUsuario.setString(2, personal.getCpf());
+            comandoUsuario.setString(3, personal.getEmail());
+            comandoUsuario.setDate(4, personal.getDataNasc());
+            comandoUsuario.setString(5, personal.getSenha());
+            comandoUsuario.setString(6, personal.getTipo());
+            comandoUsuario.setInt(7, personal.getId());
+
+            comandoUsuario.executeUpdate();
+
+            //Parte de atualizar a tabela personal
+            PreparedStatement comandoPersonal = conexao.prepareStatement(sqlAlterarPersonal);
+
+            comandoPersonal.setString(1, personal.getEspecialidade());
+            comandoPersonal.setInt(2, personal.getId());
+
+            comandoPersonal.executeUpdate();
+
+            System.out.println("Alteração realizada com sucesso!");
+
+            comandoUsuario.close();
+            comandoPersonal.close();
+            
+        } catch (SQLException e) {
+            System.out.println("Erro na alteracao: " + e.getMessage());
+            e.printStackTrace();
+        }
         
     }
 }
