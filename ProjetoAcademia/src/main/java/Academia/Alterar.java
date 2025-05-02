4f49829c3f16031ccdc1805b4ce1d610c42a7a33
Matheus Felipe <matheus.bastos@estudante.ifgoiano.edu.br>
2025-05-02 15:26:45 -0300

    Criação do Método de Alterar ADM
    Criei o método alterarADM, que altera e salva esses dados de ADM no banco de dados.

--- ProjetoAcademia/src/main/java/Academia/Alterar.java
+++ ProjetoAcademia/src/main/java/Academia/Alterar.java
@@ -21,9 +21,9 @@ public class Alterar {
         aluno.setTipo(tipo);
     }
 
-    public void alterarPersonal(Personal personal, int id, String nome, String cpf, String email, String dataNasc, 
+    public void alterarPersonal(Personal personal, int id, String nome, String cpf, String email, String dataNasc,
             String senha, String tipo, String especialidade) {
-        
+
         personal.setNome(nome);
         personal.setCpf(cpf);
         personal.setEmail(email);
@@ -31,18 +31,18 @@ public class Alterar {
         personal.setSenha(senha);
         personal.setTipo(tipo);
         personal.setEspecialidade(especialidade);
-        
+
     }
-    
+
     public void alterarPersonal(Personal personal) throws SQLException {
-        
+
         Connection conexao = new Conexao().getConexao();
-        
-        String sqlAlterar = "UPDATE  usuarios  SET nome=?, cpf=?, email=?, data_nascimento=?, senha=?, tipo=? WHERE  usuario.id=?";
+
+        String sqlAlterar = "UPDATE  usuarios SET nome=?, cpf=?, email=?, data_nascimento=?, senha=?, tipo=? WHERE  usuario.id=?";
         String sqlAlterarPersonal = "UPDATE personal SET especialidade=? WHERE fk_usu_personal=?";
-        
+
         try {
-            
+
             //Parte de atualizar a tabela usuários
             PreparedStatement comandoUsuario = conexao.prepareStatement(sqlAlterar);
 
@@ -64,15 +64,57 @@ public class Alterar {
 
             comandoPersonal.executeUpdate();
 
-            System.out.println("Alteração realizada com sucesso!");
+            System.out.println("Alteracao realizada com sucesso!");
 
             comandoUsuario.close();
             comandoPersonal.close();
+
+        } catch (SQLException e) {
+            System.out.println("Erro na alteracao: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+
+    public void alterarAdm(Adm adm) throws SQLException {
+        Connection conexao = new Conexao().getConexao();
+
+        String sqlAlterar = "UPDATE usuarios SET nome=?, cpf=?, email=?, data_nascimento=?, senha=?, tipo=? WHERE usuario.id=?";
+        String sqlAlterarAdm = "UPDATE adm SET telefone_comercial=?, setor=?, descricao=? WHERE fk_usu_adm=?";
+
+        try {
+            
+            // Parte de atualizar tabela usuários
+            PreparedStatement comandoUsuario = conexao.prepareStatement(sqlAlterar);
+            
+            comandoUsuario.setString(1, adm.getNome());
+            comandoUsuario.setString(2, adm.getCpf());
+            comandoUsuario.setString(3, adm.getEmail());
+            comandoUsuario.setDate(4, adm.getDataNasc());
+            comandoUsuario.setString(5, adm.getSenha());
+            comandoUsuario.setString(6, adm.getTipo());
+            comandoUsuario.setInt(7, adm.getId());
+            
+            comandoUsuario.executeUpdate();
+
+            // Parte de atualizar tabela ADM
+            PreparedStatement comandoAdm = conexao.prepareStatement(sqlAlterarAdm);
+            
+            comandoAdm.setString(1, adm.getNumero_comercial());
+            comandoAdm.setString(2, adm.getSetor());
+            comandoAdm.setString(3, adm.getDesc());
+            comandoAdm.setInt(4, adm.getId());
+            
+            comandoAdm.executeUpdate();
+
+            System.out.println("Alteracao realizada com sucesso!");
+
+            comandoUsuario.close();
+            comandoAdm.close();
             
         } catch (SQLException e) {
             System.out.println("Erro na alteracao: " + e.getMessage());
             e.printStackTrace();
         }
-        
     }
 }
