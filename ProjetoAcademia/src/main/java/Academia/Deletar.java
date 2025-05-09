ed9575f9921f2254bb0ec6986bcb7620b44328fe
samueloli22 <samuelantuneso@icloud.com>
2025-05-09 12:58:47 -0300

    Preocauçao metodos deletar
    apagando em tudo por preocaução

--- ProjetoAcademia/src/main/java/Academia/Deletar.java
+++ ProjetoAcademia/src/main/java/Academia/Deletar.java
@@ -14,14 +14,32 @@ public class Deletar {
     
     public void deletarAluno(String email) throws SQLException{
         Connection conexao = new Conexao().getConexao();
-        String sql_delA = "DELETE financa, assinatura, usuario, aluno FROM usuario LEFT JOIN financa ON financa.usuario_id = usuario.id LEFT JOIN assinatura ON assinatura.aluno_id = usuario.id LEFT JOIN aluno ON aluno.fk_usu_aluno = usuario.id WHERE usuario.email = ? AND usuario.tipo = 'aluno'; ";
-            
-        PreparedStatement comando_del = conexao.prepareStatement(sql_delA);
-        comando_del.setString(1, email);
-        comando_del.executeUpdate();
         
-        System.out.println("Aluno excluido com sucesso");
-        comando_del.close();
+        String sql_delFinanca = "DELETE FROM financa WHERE usuario_id = (SELECT id FROM usuario WHERE email = ? AND tipo = 'aluno')";
+        PreparedStatement comando_delFinanca = conexao.prepareStatement(sql_delFinanca);
+        comando_delFinanca.setString(1, email);
+        comando_delFinanca.executeUpdate();
+        
+        
+        String sql_delAssinatura = "DELETE FROM assinatura WHERE aluno_id = (SELECT id FROM usuario WHERE email = ? AND tipo = 'aluno')";
+        PreparedStatement comando_delAssinatura = conexao.prepareStatement(sql_delAssinatura);
+        comando_delAssinatura.setString(1, email);
+        comando_delAssinatura.executeUpdate();
+
+        
+        String sql_delAluno = "DELETE FROM aluno WHERE fk_usu_aluno = (SELECT id FROM usuario WHERE email = ? AND tipo = 'aluno')";
+        PreparedStatement comando_delAluno = conexao.prepareStatement(sql_delAluno);
+        comando_delAluno.setString(1, email);
+        comando_delAluno.executeUpdate();
+
+        
+        String sql_delUsuario = "DELETE FROM usuario WHERE email = ? AND tipo = 'aluno'";
+        PreparedStatement comando_delUsuario = conexao.prepareStatement(sql_delUsuario);
+        comando_delUsuario.setString(1, email);
+        comando_delUsuario.executeUpdate();
+        
+        
+        System.out.println("Aluno excluído com sucesso");
                         
        
         
@@ -29,13 +47,32 @@ public class Deletar {
     
      public void deletarPersonal(String email) throws SQLException{
         Connection conexao = new Conexao().getConexao();
-        String sql_delP = "DELETE financa, assinatura, usuario, personal FROM usuario LEFT JOIN financa ON financa.usuario_id = usuario.id LEFT JOIN assinatura ON assinatura.aluno_id = usuario.id LEFT JOIN personal ON personal.fk_usu_personal = usuario.id WHERE usuario.email = ? AND usuario.tipo = 'personal';";
-        PreparedStatement comando_del1 = conexao.prepareStatement(sql_delP);
-        comando_del1.setString(1, email);
-        comando_del1.executeUpdate();
+        String sql_delF = "DELETE FROM financa WHERE usuario_id = (SELECT id FROM usuario WHERE email = ? AND tipo = 'personal')";
+        PreparedStatement comando_delF = conexao.prepareStatement(sql_delF);
+        comando_delF.setString(1, email);
+        comando_delF.executeUpdate();
+
+       
+        String sql_delA = "DELETE FROM assinatura WHERE aluno_id = (SELECT id FROM usuario WHERE email = ? AND tipo = 'personal')";
+        PreparedStatement comando_delA = conexao.prepareStatement(sql_delA);
+        comando_delA.setString(1, email);
+        comando_delA.executeUpdate();
+
         
-        System.out.println("Personal excluido com sucesso");
-        comando_del1.close();
+        String sql_delP = "DELETE FROM personal WHERE fk_usu_personal = (SELECT id FROM usuario WHERE email = ? AND tipo = 'personal')";
+        PreparedStatement comando_delP = conexao.prepareStatement(sql_delP);
+        comando_delP.setString(1, email);
+        comando_delP.executeUpdate();
+
+        
+        String sql_delU = "DELETE FROM usuario WHERE email = ? AND tipo = 'personal'";
+        PreparedStatement comando_delU = conexao.prepareStatement(sql_delU);
+        comando_delU.setString(1, email);
+        comando_delU.executeUpdate();
+
+        
+        System.out.println("Personal excluído com sucesso");
+        
                         
        
         
@@ -61,11 +98,13 @@ public class Deletar {
          PreparedStatement comando_dellu = conexao.prepareStatement(sql_delU);
          comando_dellu.setString(1, email);
          comando_dellu.executeUpdate();
+         System.out.println("Personal excluído com sucesso");
          
          
      } catch(SQLException e){
          conexao.rollback(); // Reverte em caso de erro
          e.printStackTrace();
+          System.out.println("falha");
          
          
      }finally{
