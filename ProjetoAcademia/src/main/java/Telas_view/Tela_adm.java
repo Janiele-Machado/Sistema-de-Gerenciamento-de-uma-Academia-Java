ff5cfefa508a1e609a4872a8dbce29290c522f39
samueloli22 <samuelantuneso@icloud.com>
2025-06-14 14:53:23 -0300

    Update Tela_adm.java

--- ProjetoAcademia/src/main/java/Telas_view/Tela_adm.java
+++ ProjetoAcademia/src/main/java/Telas_view/Tela_adm.java
@@ -156,6 +156,7 @@ public class Tela_adm extends javax.swing.JFrame {
         // TODO add your handling code here:
         Tela_listagemPersonal tl = new Tela_listagemPersonal();
        tl.setVisible(true);
+       this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
